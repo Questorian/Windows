2b97fe21987cd7a195aaa59d7e4227d3f5aa6431
fjb <developer@QuestorSystems.com>
2012-07-23 00:38:26 +0200

    first rework on PS 2.0 (Windows 7) host.

--- PowerShell/01-Learning/01-intro.ps1
+++ PowerShell/01-Learning/01-intro.ps1
@@ -1,3 +1,89 @@
+# SV-B319 - PowerShell for Beginners - Jeffery Snover & Travis Jones
+# 2012-05-28
 
 
-get-process
+# NOTE: this script is designed to work with PowerShell 3.0!
+# Some of the cmdlets in this script are only available starting PS 3.0. Thus
+# check the version of PowerShell you are running under with the command:
+#
+#   $host.version
+# 
+
+
+
+# COMMENTS
+# --------
+# comments are currently single line comments and there appear to be no block 
+# comments in PS yet
+# '#' hash sigh = a comment - Just like Perl (bash, etc)
+
+
+# get-process applet
+# ------------------
+# comments can go after the command too ...
+get-process     # return all information on all running processes
+
+
+
+get-process -name svchost       # find SVCHOST processes only
+
+get-process -n svchost          # will match first uique parameter
+                                # first not ambiguous parameter
+
+get-process svchost             # find SVCHOST processes only
+
+get-process svcho*              # SVCHost - with wildcards
+
+gps -name svchost               # use a shell alias - gps -> get-process
+
+gps -n svc*                     # everything abbreviated 
+                                # plus - wild cards for parameters
+
+gps svc*                        # positional parameters
+
+# ALIAS
+# -----
+# Same as the bash, sh, alias functions - very handy for command line action
+
+alias           # see all current aliases
+
+
+# SORT clause
+# -----------
+# Very useful to get the relevant data implemented as a filter
+
+get-process | sort handles          # sort process list by handle count
+
+get-process | sort handles -desc    # process by descending handle count
+
+gps | sort id                       # processes sorted by PID - ascending
+
+gps | sort workingset               # processes by workingset memory usage
+
+
+gps | sort workingset -desc         # workingset memory usage - top-down
+
+gps | sort -desc workingset         # workingset memory - top-down (better way)
+
+
+
+# WHERE clause
+# ------------
+# pipe data into WHERE clauses 
+
+# get-process | where handles -ge 900 - currently does not work!
+
+
+# VERSION
+# Finding the current PowerShell version
+
+$host.version       # use builtin variable '$host'
+
+get-host            # call this applet to return version and other stuff
+
+$PSVersionTable     # similar but different output to get-host
+                    # note - this seems to return different (ie more) 
+                    # information when called within a script
+
+
+
--- PowerShell/01-Learning/ps-readme.txt
+++ PowerShell/01-Learning/ps-readme.txt
@@ -11,3 +11,32 @@ First thing we need to do is enable PowerShell scripts to run - We need to set t
 
 You are now ready to roll - start scripting!
 
+Permissions Note:
+-----------------
+This is per user and not a system wide setting. This has always worked ok for me but recently it would not let me change the ExecutionPolicy and if I run as administration it works bu this only changes the setting for the administrator. My solution was to give myself full permissions to the key (wiht RegEdit) :
+
+    HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft
+        \PowerShell\1\ShellIds\Microsoft.PowerShell
+
+This works, but is really stupid.
+
+Running Scripts
+---------------
+Lot of Linux/Unix similarity here, but you can use the ./ syntax to run a script, but of-course in PowerShell that would be .\ (dot backslash) just to cause extra problems for those of us that use both platforms. If you use both plagorms, be aware, and pay attention!
+
+
+Growth of PowerShell
+--------------------
+The corresponding number of applets for each versino of Windows:
+WS08        130 cmdlets
+WS08R2      230 cmdlets
+WS12       2300 cmdlets
+
+All functionality in WS12 (formerly Windows Server 8) Server Manager can now be scripted with PowerShell. This is part of the so-called "Windows Engineering Criteria".
+
+
+PowerShell Versions
+-------------------
+Windows 8 and Server 2012 will ship with PowerShell 3.0.
+Windows 7 - PowerShell 2.0
+Vista - Powershell 1.0
